948b8a80feb379392f6ec875a7fd741ac18c54cc
HARSH1911RATHORE <hara6329@colorado.edu>
2020-04-19 21:52:26 -0600

    load and unload module

---
+++ base_external/package/temp/temp.mk
@@ -0,0 +1,21 @@
+
+##############################################################
+#
+# TEMP
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+TEMP_VERSION ='8e9a978114c1ecbc2c7d53d55c2a277401ad6dd4'
+TEMP_SITE ='git@github.com:cu-ecen-5013/final-project-Kushagra42.git'
+TEMP_SITE_METHOD =git
+
+
+
+TEMP_MODULE_SUBDIRS=/temp
+
+
+
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
