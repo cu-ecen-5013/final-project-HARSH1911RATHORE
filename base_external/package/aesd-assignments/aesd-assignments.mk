05fb4c1887ae8d6bc9d394bb25c45784d680e0f4
HARSH1911RATHORE <hara6329@colorado.edu>
2020-04-23 22:57:23 -0600

    temp.c modified

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION =64dc7e0ce59157454a0a7775f43272788ad68324
+AESD_ASSIGNMENTS_VERSION =6685694a0eee7d226843accbb3e49643c883e836
 AESD_ASSIGNMENTS_SITE =git@github.com:cu-ecen-5013/final-project-Kushagra42.git
 AESD_ASSIGNMENTS_SITE_METHOD =git
 
@@ -18,7 +18,7 @@ endef
 #TODO: Add required executables or scripts below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/Temperature_i2c $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/Uart_raspi $(TARGET_DIR)/bin
+#	$(INSTALL) -m 0755 $(@D)/Uart_raspi $(TARGET_DIR)/bin
 endef
 
 
--- base_external/package/temp/temp.mk
+++ base_external/package/temp/temp.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-TEMP_VERSION ='64dc7e0ce59157454a0a7775f43272788ad68324'
+TEMP_VERSION ='6685694a0eee7d226843accbb3e49643c883e836'
 TEMP_SITE ='git@github.com:cu-ecen-5013/final-project-Kushagra42.git'
 TEMP_SITE_METHOD =git
 
